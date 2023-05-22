8c74b2b48fdfe932a07cd16491b517cba426893f
NoelleFOFA <105308830+NoelleFOFA@users.noreply.github.com>
2023-05-22 17:48:17 -0300

    ver4

--- plataforma/Assets/Scripts/Inimigos.cs
+++ plataforma/Assets/Scripts/Inimigos.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Inimigos : MonoBehaviour
 {
     public float velocidadeInimigo;
+    public GameObject tiroinimigo;
+    public Transform localdisparoinimigo;
+    public float tempomaxtiro;
+    public float tempoatual;
+    public bool inimigoatirador;
+
     void Start()
     {
         
@@ -12,9 +18,22 @@ public class Inimigos : MonoBehaviour
     void Update()
     {
         MovimentoInimigo();
+        if(inimigoatirador == true)
+        {
+            Atirarinimigos();
+        }
     }
     private void MovimentoInimigo()
     {
         transform.Translate(Vector3.up * velocidadeInimigo * Time.deltaTime);
+    }
+    private void Atirarinimigos()
+    {
+        tempoatual -= Time.deltaTime;
+        if(tempoatual <= 0)
+        {
+            Instantiate(tiroinimigo, localdisparoinimigo.position, Quaternion.Euler(0f,0f,0f));
+            tempoatual = tempomaxtiro;
+        }
     }
 }
